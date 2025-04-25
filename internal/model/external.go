b70de54210a66e7e5e9b7d336a0bf469458ef2e1
bagusbpg <bagusbpg@gmail.com>
2025-04-25 08:16:55 +0700

    fix certificate usage

--- internal/model/external.go
+++ internal/model/external.go
@@ -9,7 +9,6 @@ import (
 
 type MSGraphAuth struct {
 	AccessToken      string  `json:"access_token"`
-	RefreshToken     string  `json:"refresh_token"`
 	IDToken          string  `json:"id_token"`
 	TokenType        string  `json:"token_type"`
 	ExpiresIn        int64   `json:"expires_in"`
