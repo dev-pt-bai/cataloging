f780534afac500fe13f1fb0cfcea934c08784d93
bagusbpg <bagusbpg@gmail.com>
2025-09-10 21:33:19 +0700

    modify excel package

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.24
 require (
 	github.com/go-sql-driver/mysql v1.9.2
 	github.com/redis/go-redis/v9 v9.8.0
-	github.com/xuri/excelize/v2 v2.9.1
 	golang.org/x/crypto v0.38.0
 	golang.org/x/sync v0.14.0
 	golang.org/x/time v0.11.0
@@ -15,11 +14,4 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/richardlehane/mscfb v1.0.4 // indirect
-	github.com/richardlehane/msoleps v1.0.4 // indirect
-	github.com/tiendc/go-deepcopy v1.6.0 // indirect
-	github.com/xuri/efp v0.0.1 // indirect
-	github.com/xuri/nfp v0.0.1 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
 )
--- internal/pkg/excel/excel.go
+++ internal/pkg/excel/excel.go
@@ -1,10 +1,14 @@
 package excel
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/xml"
+	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/dev-pt-bai/cataloging/internal/pkg/errors"
-	"github.com/xuri/excelize/v2"
 )
 
 type Parser struct{}
@@ -12,15 +16,118 @@ type Parser struct{}
 func NewParser() *Parser { return &Parser{} }
 
 func (p *Parser) Open(r io.Reader) ([][]string, *errors.Error) {
-	file, err := excelize.OpenReader(r)
+	data, err := io.ReadAll(r)
 	if err != nil {
 		return nil, errors.New(errors.ParsingFileFailure).Wrap(err)
 	}
-	defer file.Close()
 
-	rows, err := file.GetRows(file.GetSheetName(file.GetActiveSheetIndex()))
+	zr, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
 	if err != nil {
 		return nil, errors.New(errors.ParsingFileFailure).Wrap(err)
+	}
+
+	var sharedStringsFile, sheetFile *zip.File
+	for _, f := range zr.File {
+		if sharedStringsFile != nil && sheetFile != nil {
+			break
+		}
+		switch f.Name {
+		case "xl/sharedStrings.xml":
+			sharedStringsFile = f
+		case "xl/worksheets/sheet1.xml":
+			sheetFile = f
+		}
+	}
+
+	sharedStrings, err := parseSharedStrings(sharedStringsFile)
+	if err != nil {
+		return nil, errors.New(errors.ParsingFileFailure).Wrap(err)
+	}
+
+	sheet, err := parseSheet(sheetFile, sharedStrings)
+	if err != nil {
+		return nil, errors.New(errors.ParsingFileFailure).Wrap(err)
+	}
+
+	return sheet, nil
+}
+
+type sharedStrings struct {
+	XMLName xml.Name     `xml:"sst"`
+	Items   []sharedItem `xml:"si"`
+}
+
+type sharedItem struct {
+	T string `xml:"t"`
+}
+
+func parseSharedStrings(zf *zip.File) ([]string, error) {
+	if zf == nil {
+		return nil, nil
+	}
+
+	rc, err := zf.Open()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open zip file of shared strings: %w", err)
+	}
+	defer rc.Close()
+
+	ss := new(sharedStrings)
+	if err := xml.NewDecoder(rc).Decode(ss); err != nil {
+		return nil, fmt.Errorf("failed to decode xml of shared strings: %w", err)
+	}
+
+	values := make([]string, len(ss.Items))
+	for i, item := range ss.Items {
+		values[i] = item.T
+	}
+
+	return values, nil
+}
+
+type cell struct {
+	R string `xml:"r,attr"`
+	T string `xml:"t,attr"`
+	V string `xml:"v"`
+}
+
+type row struct {
+	Cells []cell `xml:"c"`
+}
+
+type sheet struct {
+	Rows []row `xml:"sheetData>row"`
+}
+
+func parseSheet(zf *zip.File, shared []string) ([][]string, error) {
+	if zf == nil {
+		return nil, nil
+	}
+
+	rc, err := zf.Open()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open zip file of sheet: %w", err)
+	}
+	defer rc.Close()
+
+	s := new(sheet)
+	if err := xml.NewDecoder(rc).Decode(s); err != nil {
+		return nil, fmt.Errorf("failed to decode xml of sheet: %w", err)
+	}
+
+	rows := make([][]string, len(s.Rows))
+	for i, row := range s.Rows {
+		rows[i] = make([]string, len(row.Cells))
+		for j, cell := range row.Cells {
+			value := cell.V
+			if cell.T == "s" {
+				idx, err := strconv.Atoi(cell.V)
+				if err == nil && idx < len(shared) {
+					value = shared[idx]
+				}
+			}
+			rows[i][j] = value
+		}
 	}
 
 	return rows, nil
